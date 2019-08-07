80e8a384713fa9e497735a792a096fc4dcd26223
Coady LaCroix <clacroix@redhat.com>
2019-08-06 17:10:28 -0700

    Removed heavy use of global variables and redesigned fixtures used by TestOCSBasics test class
    Signed-off-by: Coady LaCroix <clacroix@redhat.com>

--- tests/manage/pv_services/test_create_storage_class_pvc.py
+++ tests/manage/pv_services/test_create_storage_class_pvc.py
@@ -8,121 +8,156 @@ from tests import helpers
 log = logging.getLogger(__name__)
 
 
-@pytest.fixture(scope='function')
-def test_fixture_rbd(request):
-    request.addfinalizer(teardown_rbd)
-    setup_rbd()
-
-
-def setup_rbd():
+@pytest.fixture()
+def ceph_block_pool(request):
     """
-    Setting up the environment
-    Creating replicated pool,secret,storageclass for rbd
+    Create and return ceph block pool
     """
-    log.info("Creating CephBlockPool")
-    global RBD_POOL
-    RBD_POOL = helpers.create_ceph_block_pool()
-    global RBD_SECRET_OBJ
-    RBD_SECRET_OBJ = helpers.create_secret(constants.CEPHBLOCKPOOL)
-    global RBD_SC_OBJ
-    log.info("Creating RBD Storage class ")
-    RBD_SC_OBJ = helpers.create_storage_class(
-        interface_type=constants.CEPHBLOCKPOOL,
-        interface_name=RBD_POOL.name,
-        secret_name=RBD_SECRET_OBJ.name
-    )
+    def fin():
+        log.info("Deleting ceph block pool %s", block_pool.name)
+        block_pool.delete()
+
+    request.addfinalizer(fin)
+
+    log.info("Creating ceph block pool")
+    block_pool = helpers.create_ceph_block_pool()
+    return block_pool
 
 
-def teardown_rbd():
+@pytest.fixture()
+def create_secret(request):
     """
-    Tearing down the environment
-    Deleting pod,replicated pool,pvc,storageclass,secret of rbd
+    Factory fixture for creating secrets
     """
-    global RBD_PVC_OBJ, RBD_POD_OBJ
-    log.info('deleting rbd pod')
-    RBD_POD_OBJ.delete()
-    log.info("Deleting RBD PVC")
-    RBD_PVC_OBJ.delete()
-    assert helpers.validate_pv_delete(RBD_PVC_OBJ.backed_pv)
-    log.info("Deleting CEPH BLOCK POOL")
-    RBD_POOL.delete()
-    log.info("Deleting RBD Secret")
-    RBD_SECRET_OBJ.delete()
-    log.info("Deleting RBD Storageclass")
-    RBD_SC_OBJ.delete()
+    secrets = []
+
+    def fin():
+        for secret in secrets:
+            log.info("Deleting secret %s", secret.name)
+            secret.delete()
+
+    request.addfinalizer(fin)
+
+    def _create_secret(interface_type):
+        log.info("Creating secret with interface type %s", interface_type)
+        secret = helpers.create_secret(interface_type)
+        secrets.append(secret)
+        return secret
+
+    return _create_secret
 
 
-@pytest.fixture(scope='function')
-def test_fixture_cephfs(request):
+@pytest.fixture()
+def create_storage_class(request, ceph_block_pool, create_secret):
+    """
+    Factory fixture for creating storage classes
+    """
+    storage_classes = []
 
-    request.addfinalizer(teardown_fs)
-    setup_fs()
+    def fin():
+        for storage_class in storage_classes:
+            log.info("Deleting storage class %s", storage_class.name)
+            storage_class.delete()
+
+    request.addfinalizer(fin)
+
+    def _create_storage_class(interface_type):
+        block_pool = ceph_block_pool
+        secret = create_secret(interface_type)
+
+        log.info(
+            "Creating storage class with interface type %s", interface_type
+        )
+        interface_name = None
+        if interface_type == constants.CEPHBLOCKPOOL:
+            interface_name = block_pool.name
+        elif interface_type == constants.CEPHFILESYSTEM:
+            interface_name = helpers.get_cephfs_data_pool_name()
+        storage_class = helpers.create_storage_class(
+            interface_type=interface_type,
+            interface_name=interface_name,
+            secret_name=secret.name
+        )
+        storage_classes.append(storage_class)
+        return storage_class
+
+    return _create_storage_class
 
 
-def setup_fs():
-    log.info("Creating CEPHFS Secret")
-    global CEPHFS_SECRET_OBJ
-    CEPHFS_SECRET_OBJ = helpers.create_secret(constants.CEPHFILESYSTEM)
+@pytest.fixture()
+def create_pod(request):
+    """
+    Factory fixture for creating pods
+    """
+    pods = []
 
-    global CEPHFS_SC_OBJ
-    log.info("Creating CephFS Storage class ")
-    CEPHFS_SC_OBJ = helpers.create_storage_class(
-        constants.CEPHFILESYSTEM,
-        helpers.get_cephfs_data_pool_name(),
-        CEPHFS_SECRET_OBJ.name
-    )
+    def fin():
+        for pod in pods:
+            log.info("Deleting pod %s", pod.name)
+            pod.delete()
+
+    request.addfinalizer(fin)
+
+    def _create_pvc(interface_type, pvc_name):
+        log.info("Creating Pod with PVC %s", pvc_name)
+        pod = helpers.create_pod(
+            interface_type=interface_type, pvc_name=pvc_name
+        )
+        pods.append(pod)
+        return pod
+
+    return _create_pvc
 
 
-def teardown_fs():
-    global CEPHFS_PVC_OBJ, CEPHFS_POD_OBJ
-    log.info('deleting cephfs pod')
-    CEPHFS_POD_OBJ.delete()
-    log.info('deleting cephfs pvc')
-    CEPHFS_PVC_OBJ.delete()
-    assert helpers.validate_pv_delete(CEPHFS_PVC_OBJ.backed_pv)
-    log.info("Deleting CEPHFS Secret")
-    CEPHFS_SECRET_OBJ.delete()
-    log.info("Deleting CephFS Storageclass")
-    CEPHFS_SC_OBJ.delete()
+@pytest.fixture()
+def create_pvc(request, create_storage_class):
+    """
+    Factory fixture for creating PVCs
+    """
+    pvcs = []
+
+    def fin():
+        for pvc in pvcs:
+            log.info("Deleting PVC %s", pvc.name)
+            pvc.delete()
+            assert helpers.validate_pv_delete(pvc.backed_pv)
+
+    request.addfinalizer(fin)
+
+    def _create_pvc(interface_type, pvc_name):
+        log.info("Creating PVC %s", pvc_name)
+        storage_class = create_storage_class(interface_type)
+        pvc = helpers.create_pvc(
+            sc_name=storage_class.name, pvc_name=pvc_name
+        )
+        pvcs.append(pvc)
+        return pvc
+
+    return _create_pvc
 
 
 @tier1
-class TestOSCBasics(ManageTest):
+class TestOCSBasics(ManageTest):
     @pytest.mark.polarion_id("OCS-336")
-    def test_basics_rbd(self, test_fixture_rbd):
+    def test_basics_rbd(self, create_pvc, create_pod):
         """
         Testing basics: secret creation,
         storage class creation,pvc and pod with rbd
         """
-        global RBD_PVC_OBJ, RBD_POD_OBJ
-        log.info('creating pvc for RBD ')
-        pvc_name = helpers.create_unique_resource_name(
-            'test-rbd', 'pvc'
-        )
-        RBD_PVC_OBJ = helpers.create_pvc(
-            sc_name=RBD_SC_OBJ.name, pvc_name=pvc_name
-        )
-        if RBD_PVC_OBJ.backed_pv is None:
-            RBD_PVC_OBJ.reload()
-        RBD_POD_OBJ = helpers.create_pod(
-            interface_type=constants.CEPHBLOCKPOOL, pvc_name=RBD_PVC_OBJ.name
-        )
+        pvc_name = helpers.create_unique_resource_name('test-rbd', 'pvc')
+        interface_type = constants.CEPHBLOCKPOOL
+        pvc = create_pvc(interface_type, pvc_name)
+        if pvc.backed_pv is None:
+            pvc.reload()
+        create_pod(interface_type, pvc.name)
 
     @pytest.mark.polarion_id("OCS-346")
-    def test_basics_cephfs(self, test_fixture_cephfs):
+    def test_basics_cephfs(self, create_pvc, create_pod):
         """
         Testing basics: secret creation,
          storage class creation, pvc and pod with cephfs
         """
-        global CEPHFS_PVC_OBJ, CEPHFS_POD_OBJ
-        log.info('creating pvc for CephFS ')
-        pvc_name = helpers.create_unique_resource_name(
-            'test-cephfs', 'pvc'
-        )
-        CEPHFS_PVC_OBJ = helpers.create_pvc(
-            sc_name=CEPHFS_SC_OBJ.name, pvc_name=pvc_name
-        )
-        log.info('creating cephfs pod')
-        CEPHFS_POD_OBJ = helpers.create_pod(
-            interface_type=constants.CEPHFILESYSTEM, pvc_name=CEPHFS_PVC_OBJ.name
-        )
+        pvc_name = helpers.create_unique_resource_name('test-cephfs', 'pvc')
+        interface_type = constants.CEPHFILESYSTEM
+        pvc = create_pvc(interface_type, pvc_name)
+        create_pod(interface_type, pvc.name)
