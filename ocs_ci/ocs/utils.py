3b427165f831fe1a61d1dfd70dd4c1c1b9e44902
Coady LaCroix <clacroix@redhat.com>
2023-05-19 00:01:46 -0700

    Support multiple ACM hubs (#7602)
    * Support multiple ACM hubs
    
    Signed-off-by: Coady LaCroix <clacroix@redhat.com>

--- ocs_ci/ocs/utils.py
+++ ocs_ci/ocs/utils.py
@@ -1292,7 +1292,7 @@ def get_non_acm_cluster_config():
     """
     non_acm_list = []
     for i in range(len(ocsci_config.clusters)):
-        if i == ocsci_config.get_acm_index():
+        if i == ocsci_config.get_active_acm_index():
             continue
         else:
             non_acm_list.append(ocsci_config.clusters[i])
